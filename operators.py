11763b08e92154bb6728b94d4e8875d024a98788
Jamesson Faria <jamessonjr@gmail.com>
2021-02-09 01:56:07 +0000

    adjust main and add variables and operators

---
+++ operators.py
@@ -0,0 +1,12 @@
+n1 = 20
+n2 = 30
+
+s1 = "james"
+s2 = "100"
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 / n2)
+print(n1 * n2)
+
+print(s1 + s2)
---
+++ variaveis.py
@@ -0,0 +1,36 @@
+### variables
+a = 10
+b = 20
+c = 30
+print("Hello World")
+
+year = 2021
+age = 41
+print(year - age)
+
+### data types
+# numbers = 3, 100, -20, 10.50, 50
+# strings = "Jamesson", "3", "python", "maratona"
+# boolean = True, False 
+
+a = 10
+name = "Jamesson"
+texto = "that's my life"
+texto1 = "imprime as \"\""
+print(texto1)
+
+status = True
+print(status)
+status = False
+print(status)
+
+a_float = 10.99
+print(a_float)
+print(type(a_float))
+print(type(a))
+print(type(texto))
+print(type(status))
+
+
+
+
