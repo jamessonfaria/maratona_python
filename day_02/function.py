50c0831efa16be5de0cb020de0e68b402d4caa2f
Jamesson Faria <jamessonjr@gmail.com>
2021-02-09 22:45:58 +0000

    update functions

--- day_02/function.py
+++ day_02/function.py
@@ -2,5 +2,23 @@ def welcome(name):
   msg = "Olá " + name
   print(msg)
 
+aluno = "james"
+welcome(aluno)
 
-welcome("james")
+def plus(n1=0, n2=0):
+  print(int(n1) + int(n2))
+
+def minus(n1=0, n2=0):
+  print(int(n1) - int(n2))
+
+n1 = input("Digite o N1")
+n2 = input("Digite o N2")
+plus(n1, n2)
+minus(n1, n2)
+
+plus(10)
+
+def interpolacao(name):
+  print(f"Olá usuario: {name}")
+
+interpolacao("Carlos")
