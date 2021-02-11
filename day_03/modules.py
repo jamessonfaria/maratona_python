e8e15ab97fd39b47b3541d566251d2c71384a0e3
Jamesson Faria <jamessonjr@gmail.com>
2021-02-11 02:26:15 +0000

    import other modules

--- day_03/modules.py
+++ day_03/modules.py
@@ -1,6 +1,10 @@
 from day_03.calc import *
+import humanizer_portugues
 
 print(add(1,2))
 print(subtract(10,20))
 print(multiply(10,10))
 print(divide(30,3))
+
+result = 13123123312
+print(humanizer_portugues.int_word(result))
