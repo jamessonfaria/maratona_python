4139511e0958f3fdcc9b37357fc4bb2618c5b3ca
Jamesson Faria <jamessonjr@gmail.com>
2021-02-11 01:53:11 +0000

    adjust flow

--- day_03/flow.py
+++ day_03/flow.py
@@ -1,11 +1,11 @@
-def unlock_adult_movies(username, age):
-  if age < 18:
+def unlock_adult_movies(username, age, with_parents=False):
+  if age < 18 and not with_parents:
     msg = f"{username} não pode assistir filmes adultos."
   else:
     msg = f"{username} pode assistir filmes adultos."
 
   return msg
 
-print(unlock_adult_movies("lucas", 12)) 
+print(unlock_adult_movies("lucas", 12, True)) 
 print(unlock_adult_movies("jorge", 18)) 
 print(unlock_adult_movies("marcos", 25)) 
