8e2120e704b33e0b3b78660608ba7b43618b4416
Jamesson Faria <jamessonjr@gmail.com>
2021-02-12 00:43:02 +0000

    adjust request

--- day_04/request.py
+++ day_04/request.py
@@ -2,5 +2,7 @@ import requests
 
 url_indeed = "https://br.indeed.com/empregos-de-python"
 r = requests.get(url_indeed)
-
-print(r.text)
+if r.status_code == 200:
+  print("o site esta online")
+else:
+  print("o site esta off")
