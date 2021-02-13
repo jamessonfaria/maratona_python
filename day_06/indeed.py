ca43765fc59d9c8c99975b48f5eee3581c020b62
Jamesson Faria <jamessonjr@gmail.com>
2021-02-13 14:20:13 +0000

    adjust modules

---
+++ day_06/indeed.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrapping_indeed(url):
+  # url + request
+  response = requests.get(url)
+  # salva o html de retorno
+  html_response = response.text
+  # faz a sopa
+  soup = BeautifulSoup(html_response, 'html.parser')
+  # filtrar os cards
+  cards = soup.find_all('div', class_="result")
+  jobs = []
+  for card in cards:
+    job = {
+      'title': card.find('a').get('title'),
+      'company': card.find('span', class_='company').get_text(),
+      'location': card.find('span', class_='location').string,
+      'how_old': card.find('span', class_='date').string,
+      'link': f"https://br.indeed.com{card.find('a').get('href')}"
+    }
+    jobs.append(job)
+  return jobs
--- day_06/web_scrapping.py
+++ day_06/web_scrapping.py
@@ -1,27 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+from day_06.indeed import scrapping_indeed
 
-def scrapping_indeed(url):
-  # url + request
-  response = requests.get(url)
-  # salva o html de retorno
-  html_response = response.text
-  # faz a sopa
-  soup = BeautifulSoup(html_response, 'html.parser')
-  # filtrar os cards
-  cards = soup.find_all('div', class_="result")
-  jobs = []
-  for card in cards:
-    job = {
-      'title': card.find('a').get('title'),
-      'company': card.find('span', class_='company').get_text(),
-      'location': card.find('span', class_='location').string,
-      'how_old': card.find('span', class_='date').string,
-      'link': f"https://br.indeed.com{card.find('a').get('href')}"
-    }
-    jobs.append(job)
-  return jobs
+res_python = scrapping_indeed("https://br.indeed.com/empregos-de-python")
+res_js = scrapping_indeed("https://br.indeed.com/empregos-de-javascript")
 
-url_indeed = "https://br.indeed.com/empregos-de-python"
-print(scrapping_indeed(url_indeed))
+print(res_python)
+print(res_js)
+
   
