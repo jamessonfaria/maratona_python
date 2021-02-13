4c8e5b742afdf7000a3c2a971632f6602a76b561
Jamesson Faria <jamessonjr@gmail.com>
2021-02-13 16:41:37 +0000

    adjust in links of page

--- day_06/indeed.py
+++ day_06/indeed.py
@@ -11,38 +11,38 @@ def search_keyword(keyword):
   soup = BeautifulSoup(html_response, 'html.parser')
   
   # descobrimos quantas paginas analisando o codigo
-  pages_links = soup.find('ul', class_="pagination-list").find_all('li')
+  pages_links = soup.find('ul', class_="pagination-list").find_all('a')
   
   #lista que armazena paginas
-  number_of_pages = [0]
+  number_of_pages = [0,1]
   for link in pages_links:
-    n_page = link.get_text()
-    number_of_pages.append(n_page)
+    n_page = link.string
+    if n_page == None:
+      continue
+    number_of_pages.append(int(n_page))
 
   urls = []
-  for n_page in number_of_pages[:-1]:
-    url = f"{url_base}q={keyword}&limit={results_per_page}&start={results_per_page * int(n_page)}"
+  for n_page in number_of_pages:
+    url = f"{url_base}q={keyword}&limit={results_per_page}&start={results_per_page * n_page}"
     urls.append(url)
 
-  return urls
+  return scrapping_indeed(urls)
 
-def scrapping_indeed(url):
-  # url + request
-  response = requests.get(url)
-  # salva o html de retorno
-  html_response = response.text
-  # faz a sopa
-  soup = BeautifulSoup(html_response, 'html.parser')
-  # filtrar os cards
-  cards = soup.find_all('div', class_="result")
-  jobs = []
-  for card in cards:
-    job = {
-      'title': card.find('a').get('title'),
-      'company': card.find('span', class_='company').get_text(),
-      'location': card.find('span', class_='location').string,
-      'how_old': card.find('span', class_='date').string,
-      'link': f"https://br.indeed.com{card.find('a').get('href')}"
-    }
-    jobs.append(job)
-  return jobs
+def scrapping_indeed(urls):
+  all_jobs = []
+  for url in urls:
+    response = requests.get(url)
+    html_response = response.text
+    soup = BeautifulSoup(html_response, 'html.parser')
+    # filtrar os cards
+    cards = soup.find_all('div', class_="result")
+    for card in cards:
+      job = {
+        'title': card.find('a').get('title'),
+        #'company': card.find('span', class_='company').get_text(),
+        'location': card.find('span', class_='location').string,
+        'how_old': card.find('span', class_='date').string,
+        'link': f"https://br.indeed.com{card.find('a').get('href')}"
+      }
+      all_jobs.append(job)
+  return all_jobs
--- day_06/web_scrapping.py
+++ day_06/web_scrapping.py
@@ -3,5 +3,6 @@ from day_06.indeed import search_keyword
 #res_python = scrapping_indeed("https://br.indeed.com/empregos-de-python")
 #res_js = scrapping_indeed("https://br.indeed.com/empregos-de-javascript")
 
-print(search_keyword("ruby"))
+print(search_keyword("python"))
+
   
