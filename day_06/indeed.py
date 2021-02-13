44b0cc316ff92133fa68a64bc1ab9103c5439b1f
Jamesson Faria <jamessonjr@gmail.com>
2021-02-13 16:18:20 +0000

    adjust indeed.py

--- day_06/indeed.py
+++ day_06/indeed.py
@@ -5,22 +5,26 @@ url_base = 'https://br.indeed.com/empregos?'
 results_per_page = 50
 
 def search_keyword(keyword):
-  # url + request
+  #1. faz a busca e descobra quantas pg de resultado
   response = requests.get(f'{url_base}q={keyword}&limit=50&start=0')
-  # salva o html de retorno
   html_response = response.text
-  # faz a sopa
   soup = BeautifulSoup(html_response, 'html.parser')
-
+  
+  # descobrimos quantas paginas analisando o codigo
   pages_links = soup.find('ul', class_="pagination-list").find_all('li')
-
-  number_of_pages = []
+  
+  #lista que armazena paginas
+  number_of_pages = [0]
   for link in pages_links:
     n_page = link.get_text()
     number_of_pages.append(n_page)
 
-  print(number_of_pages[:-1])
+  urls = []
+  for n_page in number_of_pages[:-1]:
+    url = f"{url_base}q={keyword}&limit={results_per_page}&start={results_per_page * int(n_page)}"
+    urls.append(url)
 
+  print(urls)
 
 def scrapping_indeed(url):
   # url + request
