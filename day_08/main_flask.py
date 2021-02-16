513673be4b318354ac5367e2f804e72cb4e99852
Jamesson Faria <jamessonjr@gmail.com>
2021-02-16 12:46:57 +0000

    add templates

--- day_08/main_flask.py
+++ day_08/main_flask.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask('MaratonaScrapping')
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!!!'
 
-@app.route('/login')
-def login():
-    return 'Pagina de login'
+'''
+@app.route('/<user>')
+def user(user):
+    return f'oi, bem vindo {user}'
+'''
+
+@app.route('/search')
+def search():
+  return render_template('search.html')
 
 app.run(host='0.0.0.0')
